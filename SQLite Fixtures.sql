f46a8797a38a5d028eb72a64e3fad5d382c66bdf
Bruno Paré-Simard <bruno.paresimard@gmail.com>
2021-04-03 20:45:36 -0400

    Recreate mrevutskyi/flask-restless-ng#13 (and maybe mrevutskyi/flask-restless-ng#1)

--- SQLite Fixtures.sql
+++ SQLite Fixtures.sql
@@ -5,7 +5,7 @@ VALUES  (1, 'ITEM B 1'),
 INSERT INTO a_item (id, name, b_item_id)
 VALUES  (1, 'ITEM A 1', 1),
         (2, 'ITEM A 2', 2),
-        (4, 'ITEM A 4', 2);
+        (3, 'ITEM A 3', 2);
 
 INSERT INTO c_item (id, name)
 VALUES  (1, 'ITEM C 1'),
